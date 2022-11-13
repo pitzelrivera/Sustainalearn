e610c90fe995b88c6a488ad8473838c785bb9166
ryanbrown535 <ryan.brown9954@gmail.com>
2022-11-13 02:58:28 -0500

    Remove redundant types.js

--- sustainalearn/server.js
+++ sustainalearn/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./db/config')
 const cors = require('cors')
 const bodyParser = require('body-parser')
-const { User, Post, Article, Tag, ArticleTag } = require('./src/types')
+const { User, Post, Article, Tag, ArticleTag } = require('./db/types')
 
 const app = express();
 app.use(cors());
