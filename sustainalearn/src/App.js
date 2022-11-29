ee628597eaec5a74ab8a2dfb626324b0594b4861
pitzelrivera <72771721+pitzelrivera@users.noreply.github.com>
2022-11-29 14:34:46 -0500

    Made article page component

--- sustainalearn/src/App.js
+++ sustainalearn/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavBar from "./NavBar";
 import Homepage from "./pages/Homepage";
 import Search from "./pages/Search";
+import Article from "./pages/Article";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './styles.css'
 import backgroundImg from "./images/background.png"
@@ -19,6 +20,7 @@ function App() {
                     <Routes>
                         <Route path={"/"} element={<Homepage />} />
                         <Route path={"/search"} element={<Search />} />
+                        <Route path={"/article"} element={<Article />} />
                     </Routes>
                 </div>
             </div>
---
+++ sustainalearn/src/pages/Article.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+const Article = () => {
+    return (
+        <h1>This is the Article Page</h1>
+    )
+}
+
+export default Article;
