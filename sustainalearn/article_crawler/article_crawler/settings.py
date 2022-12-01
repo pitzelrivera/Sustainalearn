3c87a3964219974e56f5f40d72f1bd9e9274e6ac
Alexa Cole <alexa.cole@ufl.edu>
2022-12-01 16:16:12 -0500

    Finished pipelining articles
    My task is finished

--- sustainalearn/article_crawler/article_crawler/settings.py
+++ sustainalearn/article_crawler/article_crawler/settings.py
@@ -69,9 +69,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'article_crawler.pipelines.ArticleCrawlerPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'article_crawler.pipelines.ArticleCrawlerPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
