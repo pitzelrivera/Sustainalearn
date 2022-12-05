326d641df211e4d8878355a95fdfe22ae45ffc3f
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 <yoonmgyg@gmail.com>
2022-12-04 22:03:22 -0500

    Added new google login method

--- sustainalearn/src/Login.js
+++ sustainalearn/src/Login.js
@@ -1,27 +1,57 @@
-import { GoogleLogin } from 'react-google-login';
 import './NavBar.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import jwt_decode from 'jwt-decode';
+import Axios from 'axios'
+import { User, Post, Article, Tag, ArticleTag } from "./db/types";
+import readError from "./db/errorHandle";
+
 
 const clientId = "582207653637-v3rikhn31efm2nshrtbclkq4bqogeh47.apps.googleusercontent.com";
-
-const responseGoogle = (response) => {
-  console.log(response);
-}
-
-
-<Login/>
 function Login() {
+    const [user, setUser] = useState({});
+    
+    function handleCallback(response) {
+	console.log("JWT Token:" + response.credential);
+	var userObject = jwt_decode(response.credential);
+	console.log(userObject);
+	setUser(userObject);
+	
+        const newUser = new User(user.sub, user.name, user.email, 0)
+	console.log(user.sub + user.name + user.email);
+	Axios.post("http://localhost:3001/api/createUser", { User: newUser })
+            .then(error => {
+                //console.log(error);
+                const errorMessage = readError(error);
+                console.log(errorMessage);
+            });
+    }
+
+    useEffect(() => {
+	/* global google */
+	google.accounts.id.initialize({
+	    client_id: clientId,
+	    callback: handleCallback
+	});
+	google.accounts.id.renderButton(
+	    document.getElementById("signInDiv"),
+	    { theme: "outline", size: "large"}
+	);
+	
+
+    }, []);
+    
+
     return (
-	    <div>
-	    <GoogleLogin
-	clientId={clientId}
-	render={renderProps => (
-		<button onClick={renderProps.onClick} disabled={renderProps.disabled} type="button" class="button">Login</button>
-	)}
-	onSuccess={responseGoogle}
-	    onFailure={responseGoogle}
-	    cookiePolicy={'single_host_origin'}
-	    />
-	    </div>
+	<div>
+	    <div id="signInDiv"></div>
+	    { user &&
+	      <div>
+	      <img src={user.picture}></img>
+	      <h3>{user.name}</h3>
+	      </div>
+	    }
+	</div>
     );
 }
 
